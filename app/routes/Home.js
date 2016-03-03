b7d22db329cf6abcebba2921f163d13f3f3f4014
Salman Ahmad <salman@salmanahmad.com>
2016-03-03 16:13:34 -0500

    Updates with a clicker example.

--- app/routes/Home.js
+++ app/routes/Home.js
@@ -11,24 +11,151 @@ import moment from 'moment'
 import fetch from 'isomorphic-fetch';
 import lodash from 'lodash';
 
-class Route extends React.Component {
+function clicker() {
+  alert("Hi!")
+}
 
+class Clicker extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      loading: false,
-      error: false,
-      records: Immutable.Map()
+      count: props.initialCount || 0,
+      alertMessage: null
     }
   }
 
+  increment() {
+    this.setState({
+      count: this.state.count + 1,
+      alertMessage: "Incremented!"
+    })
+
+    var self = this;
+
+    setTimeout(function() {
+      self.setState({
+        alertMessage: null
+      })
+    }, 3000)
+
+  }
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1,
+      alertMessage: "Decremented!"
+    })
+  }
+
   render() {
+
+    var alert = (<div className="alert alert-danger">{this.state.alertMessage}</div>)
+
+    if(!this.state.alertMessage) {
+      alert = null;
+    }
+
+    var template = (
+      <div>
+        <div>You have clicked: {this.state.count} times.</div>
+        {alert}
+        <button onClick={this.increment.bind(this)}>Increment</button>
+        <button onClick={this.decrement.bind(this)}>Decrement</button>
+      </div>
+    )
+
+    return template;
+
+    /*
+    var items = [
+      <li>A</li>,
+      <li>B</li>,
+      <li>C</li>
+    ]
+
+    if(Math.random() < 0.5) {
+      template = (
+        <div>Empty! Sucker!</div>
+      )
+    }
+
     return (
       <div>
-        Hello, World!
+        This is a clicker!
+        <br/>
+        {template}
+        <br/>
+        <ul>
+          {items}
+        </ul>
+        <hr/>
+      </div>
+    );
+    */
+  }
+}
+
+class Header extends React.Component {
+
+  render() {
+    var title = this.props.title;
+    if(this.props.monkey) {
+      title += " is a monkey"
+    }
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        {this.props.children}
       </div>
     )
   }
 }
 
-export default connect()(Route)
+
+class Home extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      clickers: []
+    }
+  }
+
+  add() {
+    var clickers = this.state.clickers
+    clickers.push((<Clicker />))
+    this.setState({
+      clickers: clickers
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <Header title="Clicker App" />
+          <Clicker initialCount={Math.floor(Math.random() * 50)} />
+          <br/>
+          <Clicker />
+          <br/>
+          <Clicker />
+          <br/>
+          <Clicker />
+
+          <hr/>
+
+          <h2>Dynamic List</h2>
+
+          {this.state.clickers}
+
+          <hr/>
+
+          <button onClick={this.add.bind(this)} >Add Clicker</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect()(Home)
